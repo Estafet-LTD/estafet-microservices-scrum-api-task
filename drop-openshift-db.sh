ec4a63721d9e4e895f9e2349043cceb5c66fe946
Dennis Williams <dennis.williams@estafet.com>
2018-05-01 12:21:55 +0100

    added ci pipeline and cleaned up automation.

--- drop-openshift-db.sh
+++
@@ -1,1 +0,0 @@
-psql -h $POSTGRESQL_SERVICE_HOST -p $POSTGRESQL_SERVICE_PORT -U postgres task-api < drop-task-api-db.ddl
--- start.sh
+++
@@ -1,1 +0,0 @@
-java -jar target/estafet-microservices-scrum-api-task-*.jar --server.port=9006
