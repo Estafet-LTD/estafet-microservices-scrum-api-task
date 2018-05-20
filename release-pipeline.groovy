d0ffee534c09f1b4f3c9921987bd7ee09ccb96d3
Dennis Williams <dennis.williams@estafet.com>
2018-05-20 15:02:11 +0100

    refactoered release pipeline

--- release-pipeline.groovy
+++ release-pipeline.groovy
@@ -26,6 +26,7 @@ def microservice = "task-api"
 
 def developmentVersion
 def releaseVersion
+def releaseTag
 
 node('maven') {
 
@@ -38,6 +39,7 @@ node('maven') {
 		def matcher = new XmlSlurper().parseText(pom).version =~ /(\d+\.\d+\.)(\d+)(\-SNAPSHOT)/
 		developmentVersion = "${matcher[0][1]}${matcher[0][2].toInteger()+1}-SNAPSHOT"
 		releaseVersion = "${matcher[0][1]}${matcher[0][2]}"
+		releaseTag = "v${releaseVersion}"
 	}
 	
 	stage("perform release") {
@@ -46,14 +48,25 @@ node('maven') {
         withMaven(mavenSettingsConfig: 'microservices-scrum') {
 			sh "mvn release:clean release:prepare release:perform -DreleaseVersion=${releaseVersion} -DdevelopmentVersion=${developmentVersion} -DpushChanges=false -DlocalCheckout=true -DpreparationGoals=initialize -B"
 			sh "git push origin master"
+			sh "git tag ${releaseTag}"
+			sh "git push origin ${releaseTag}"
 		} 
 	}	
-	
+
+	stage("tag image") {
+		openshiftTag namespace: project, srcStream: microservice, srcTag: 'PrepareForTesting', destinationNamespace: 'prod', destinationStream: microservice, destinationTag: releaseVersion
+	}
+
 	stage("update database") {
 		sh "oc get pods --selector app=postgresql -o json -n ${project} > pods.json"
 		def json = readFile('pods.json')
-		def pod = new groovy.json.JsonSlurper().parseText(json).items[0].metadata.name
+		def pod = new groovy.json.JsonSlurper().parseText(json).items[0].metadata.name	
+		def template = readFile ('createdb.sh.template').replaceAll(/\$\{microservice\}/, microservice)
+		writeFile file:"createdb.sh", text:template
 		sh "oc rsync --no-perms=true --include=\"*.ddl\" --exclude=\"*\" ./ ${pod}:/tmp -n ${project}"	
+		sh "oc rsync --no-perms=true --include=\"createdb.sh\" --exclude=\"*\" ./ ${pod}:/tmp -n ${project}"	
+		sh "oc exec ${pod}  -n ${project} -- /bin/sh -i -c \"chmod +x /tmp/createdb.sh\""
+		sh "oc exec ${pod}  -n ${project} -- /bin/sh -i -c \"/tmp/createdb.sh\""
 		sh "oc exec ${pod}  -n ${project} -- /bin/sh -i -c \"psql -d ${microservice} -U postgres -f /tmp/drop-${microservice}-db.ddl\""
 		sh "oc exec ${pod}  -n ${project} -- /bin/sh -i -c \"psql -d ${microservice} -U postgres -f /tmp/create-${microservice}-db.ddl\""
 	}	
@@ -72,22 +85,10 @@ node('maven') {
 		}
 		openshiftVerifyDeployment namespace: project, depCfg: microservice, replicaCount:"1", verifyReplicaCount: "true", waitTime: "600000"
 	}	
-}
-
-node('maven') {
-
-	stage("checkout acceptance tests") {
-		git branch: "master", url: "https://github.com/Estafet-LTD/estafet-microservices-scrum-qa"
-	}
-
-	stage("execute acceptance tests") {
-		withMaven(mavenSettingsConfig: 'microservices-scrum') {
-			sh "mvn clean install"
-		} 
+	
+	stage("trigger acceptance tests") {
+		sh "oc start-build qa-pipeline -n cicd"	
 	}
 	
-	stage("tag container with release version") {
-		openshiftTag namespace: project, srcStream: microservice, srcTag: 'PrepareForTesting', destinationNamespace: 'prod', destinationStream: microservice, destinationTag: releaseVersion
-	}
-
 }
+
