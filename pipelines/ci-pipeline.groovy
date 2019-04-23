26140a7000b0c7bc15a341cc3014c66da7bd4f11
Dennis Williams <dennyjoewilliams@yahoo.com>
2019-04-23 11:45:52 +0100

    add namespace project delimiter for database

--- pipelines/ci-pipeline.groovy
+++ pipelines/ci-pipeline.groovy
@@ -29,7 +29,7 @@ node("maven") {
 
 	stage("execute the container tests") {
 		withEnv(
-				[	"TASK_API_JDBC_URL=jdbc:postgresql://postgresql.${project}.svc:5432/${microservice}", 
+				[	"TASK_API_JDBC_URL=jdbc:postgresql://postgresql.${project}.svc:5432/${project}-${microservice}", 
 					"TASK_API_DB_USER=postgres", 
 					"TASK_API_DB_PASSWORD=welcome1",
 					"TASK_API_SERVICE_URI=http://${microservice}.${project}.svc:8080",
